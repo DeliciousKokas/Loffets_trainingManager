8c675c7b1b4e1d71072c58a8254e91a544568c32
Jun <pajums0235@gmail.com>
2020-01-07 16:09:23 +0900

    chage users devise_root path

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,9 @@
 Rails.application.routes.draw do
-  devise_for :users
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+  get 'pages/main'
   get '/', to:'staticpages#home'
+  get '/main', to:'pages#main'
+
+  devise_for :users
+
 end
