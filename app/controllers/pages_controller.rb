e484535ba78740b0ebce51ae2fe1dc6c4562111e
Jun <pajums0235@gmail.com>
2020-01-09 15:26:27 +0900

    create event link base

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -2,4 +2,15 @@ class PagesController < ApplicationController
   def main
     @event = Record.all
   end
+
+  def events
+    @event = Record.find_by(start_time: params[:start_time])
+
+
+    # link_to "+ Service", new_my_service_path(:my_service => { :account_id => acct.id })
+    # # my_services_controller.rb
+    # def new
+    #   @my_service = MyService.new(params[:my_service])
+    # end
+  end
 end 
