8aa1f8c7931f1d0e6962efd8819fc844cba9e6a5
Jun <pajums0235@gmail.com>
2020-02-02 11:22:40 +0900

    small fix

--- app/controllers/charts_controller.rb
+++ app/controllers/charts_controller.rb
@@ -1,15 +1,17 @@
 class ChartsController < ApplicationController
   def show
     @myworks = current_user.mywork.all
-    @mywork = Mywork.find(params[:mywork_id])
-    @workouts = Workout.where(mywork_id: @mywork)
-    
-    @chart = []
-    @workouts.each do |workout|
-      @chart.push([Record.find(workout.record_id).start_time, workout.weight])
+
+    if params[:mywork_id]
+      @mywork = Mywork.find(params[:mywork_id])
+      @workouts = Workout.where(mywork_id: @mywork)
+      
+      @chart = []
+      @workouts.each do |workout|
+        @chart.push([Record.find(workout.record_id).start_time, workout.weight])
+      end
+
+      @chart = @chart.sort
     end
-
-    @chart = @chart.sort
-
   end
 end
