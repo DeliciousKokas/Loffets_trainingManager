db26511aab67858fbd26a5aea115470dbc66b0a4
Jun <pajums0235@gmail.com>
2020-05-27 09:11:23 +0900

    Create 30days workout data by name

--- app/controllers/charts_controller.rb
+++ app/controllers/charts_controller.rb
@@ -1,11 +1,18 @@
 class ChartsController < ApplicationController
   def show
     @myworks = current_user.mywork.all
+
     @count_30days = current_user.count_30days
+
+    @mywork_30days = []
+    @myworks.each do |mywork|
+      mywork_counter = mywork.workout.count
+      @mywork_30days.push([mywork.name, mywork_counter])
+    end
     
     if params[:mywork_id]
-      @mywork = Mywork.find(params[:mywork_id])
-      @workouts = Workout.where(mywork_id: @mywork)
+
+      @workouts = Workout.where(mywork_id: Mywork.find(params[:mywork_id]))
       
       @chart = []
       @workouts.each do |workout|
